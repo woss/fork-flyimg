8f45640de7bb107f4711cf8d2f07a74c956328e5
Sadok <sadoknet@gmail.com>
2019-12-03 08:58:24 +0100

    Debug file size for unit test, issue#236

--- tests/Core/Processor/FaceDetectProcessorTest.php
+++ tests/Core/Processor/FaceDetectProcessorTest.php
@@ -18,10 +18,11 @@ class FaceDetectProcessorTest extends BaseTest
         $image = $this->imageHandler->processImage('fc_1,o_jpg,rf_1', parent::FACES_TEST_IMAGE);
         echo "\n -> ".$image->getOutputImagePath();
         echo "\n -> ".parent::FACES_CP0_TEST_IMAGE;
-        $path_parts = pathinfo($image->getOutputImagePath());
-        $path_parts2 = pathinfo(parent::FACES_CP0_TEST_IMAGE);
-        var_dump($path_parts);
-        var_dump($path_parts2);
+        $filesize = filesize($image->getOutputImagePath());
+        $filesize2 = filesize(parent::FACES_CP0_TEST_IMAGE);
+        var_dump($filesize);
+        var_dump($filesize2);
+        echo "\n -> ";
         $image1Crc32 = crc32(\file_get_contents($image->getOutputImagePath()));
         $image2Crc32 = crc32(\file_get_contents(parent::FACES_CP0_TEST_IMAGE));
         $this->generatedImage[] = $image;
