1e1a55416d193986b45eb199b442dbf42872be15
Sadok <sadoknet@gmail.com>
2020-12-30 16:49:31 +0100

    Fixing PHP_PHPCS linting error

--- constants.php
+++ constants.php
@@ -1,6 +1,5 @@
 <?php
 
-/** Define Constants && Load parameters files*/
 define('UPLOAD_WEB_DIR', 'uploads/');
 define('UPLOAD_DIR', __DIR__ . '/web/' . UPLOAD_WEB_DIR);
 define('TMP_DIR', __DIR__ . '/var/tmp/');
