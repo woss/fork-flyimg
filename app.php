9ac42817dfe4a2ecb6587c68691bd466ccf60cd5
Sadok Ferjani <sadoknet@gmail.com>
2023-08-31 19:26:37 +0200

    Update app.php

--- app.php
+++ app.php
@@ -1,11 +1,7 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-
-define('UPLOAD_WEB_DIR', 'uploads/');
-define('UPLOAD_DIR', __DIR__ . '/web/' . UPLOAD_WEB_DIR);
-define('TMP_DIR', __DIR__ . '/var/tmp/');
-define('ROOT_DIR', __DIR__);
+require_once __DIR__ . '/vendor/constants.php';
 
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
